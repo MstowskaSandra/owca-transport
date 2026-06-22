19de29939b537fd86874430ba356e6a3c5daf1b3
Sandra <sandra.mstowskaa@gmail.com>
2026-06-22 14:45:03 +0200

    style(nav): add new font and hover effects

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,23 +3,43 @@ import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 const Nav = () => {
     return (
-        <nav className="sticky top-0 z-50 bg-[#fafff4] shadow-md">
-            <div className="mx-auto px-4 py-4 sm:px-6 sm:py-4 lg:px-12 flex items-center justify-between">
-                <NavLink
-                    to="/"
-                    className="text-xl sm:text-2xl lg:text-2xl font-candal font-bold uppercase text-black-700"
-                >
-                    O.M Transport
-                </NavLink>
-            
+      <nav className="font-mont tracking-widest sticky top-0 z-50 bg-[#F1EAE6] shadow-sm">
+        <div className="mx-auto px-4 py-4 sm:px-6 sm:py-4 lg:px-12 flex items-center justify-between">
+          <NavLink
+            to="/"
+            className="text-xl sm:text-2xl lg:text-2xl tracking-wide font-extrabold uppercase text-[#353436] hover:scale-110 transition-all duration-200 hover:text-[#D46B43]"
+          >
+            O.M Transport
+          </NavLink>
 
-            <div className="hidden lg:flex space-x-8 items-center">
-                <NavLink to="/informacje">O nas</NavLink>
-                <NavLink to="/lokalizacje">Lokalizacje</NavLink>
-                <NavLink to="/kontakt">Kontakt</NavLink>
-            </div>
+          <div className="hidden font-extrabold uppercase lg:flex space-x-8 items-center">
+            <NavLink
+              to="/uslugi"
+              className="transition-all duration-200 hover:text-[#D46B43]"
+            >
+              usługi
+            </NavLink>
+            <NavLink
+              to="/informacje"
+              className="transition-all duration-200 hover:text-[#D46B43]"
+            >
+              O nas
+            </NavLink>
+            <NavLink
+              to="/lokalizacje"
+              className="transition-all duration-200 hover:text-[#D46B43]"
+            >
+              Lokalizacje
+            </NavLink>
+            <NavLink
+              to="/kontakt"
+              className="transition-all duration-200 hover:text-[#D46B43]"
+            >
+              Kontakt
+            </NavLink>
+          </div>
 
-            <div className="hidden md:flex items-center gap-6">
+          <div className="hidden md:flex items-center gap-6">
             <div className="flex items-center gap-3">
               <a
                 href="https://www.facebook.com"
@@ -41,7 +61,7 @@ const Nav = () => {
             <div className="flex flex-col items-end font-text">
               <a
                 href="tel:734414640"
-                className="text-black-700 font-mont font-bold text-sm hover:underline"
+                className="text-black font-mont font-bold text-sm hover:underline"
               >
                 734 414 640
               </a>
@@ -51,8 +71,8 @@ const Nav = () => {
             </div>
           </div>
         </div>
-        </nav>
-    )
+      </nav>
+    );
 };
 
 export default Nav;
