06b50cb4a88cab61e5392a005c79aa5c9a5ce579
Sandra <sandra.mstowskaa@gmail.com>
2026-06-30 12:47:56 +0200

    feat(ServiceCard): link cards to specific service sections using react-router-hash-link

--- src/data/servicesCards.js
+++ src/data/servicesCards.js
@@ -2,42 +2,35 @@ import { FaLuggageCart } from "react-icons/fa";
 import { LuTrash2, LuWarehouse, LuTv } from "react-icons/lu";
 import { FaTruckRampBox, FaBroom } from "react-icons/fa6";
 
-
 export const servicesCards = [
-     {
-        title: "Przeprowadzki prywatne i firmowe",
-        text: "Kompleksowa usługa od małej kawalerki po duże biuro wraz z wnoszeniem i zabezpieczeniem mienia.",
-        link: "uslugi",
-        icon: FaLuggageCart,
-    },
-    {
-        title: "Przewóz AGD i RTV",
-        text: "Bezpieczny transport sprzętu w pionie przy użyciu specjalnych pasów i mat ochronnych.",
-        link: "uslugi",
-        icon: LuTv,
-    },
-    {
-        title: "Dostawa z marketów typu IKEA & castorama",
-        text: "Szybki odbiór i transport mebli oraz materiałów budowlanych prosto pod Twoje drzwi.",
-        link: "uslugi",
-        icon: FaTruckRampBox,
-    },
-    {
-        title: "Opróżnianie mieszkań",
-        text: "Sprawne czyszczenie i przygotowanie lokali pod klucz przed sprzedażą lub wynajmem.",
-        link: "uslugi",
-        icon: LuWarehouse,
-    },
-    {
-       title: "Wywóz odpadów i gabarytów",
-       text: "Szybkie pozbywanie się starych mebli i wielkogabarytowych śmieci bez wysiłku z Twojej strony.",
-        link: "uslugi",
-        icon: LuTrash2, 
-    },
-    {
-       title: "Oczyszczanie piwnic, strychów i garaży",
-       text: "Gruntowne sprzątanie i usuwanie zbędnego wyposażenia, które pozwoli Ci odzyskać wolną przestrzeń.",
-        link: "uslugi",
-        icon: FaBroom,
-    },
-]
+  {
+    title: "Przeprowadzki prywatne i firmowe",
+    text: "Kompleksowa usługa od małej kawalerki po duże biuro wraz z wnoszeniem i zabezpieczeniem mienia.",
+    icon: FaLuggageCart,
+  },
+  {
+    title: "Przewóz AGD i RTV",
+    text: "Bezpieczny transport sprzętu w pionie przy użyciu specjalnych pasów i mat ochronnych.",
+    icon: LuTv,
+  },
+  {
+    title: "Dostawa z marketów typu IKEA & castorama",
+    text: "Szybki odbiór i transport mebli oraz materiałów budowlanych prosto pod Twoje drzwi.",
+    icon: FaTruckRampBox,
+  },
+  {
+    title: "Opróżnianie mieszkań",
+    text: "Sprawne czyszczenie i przygotowanie lokali pod klucz przed sprzedażą lub wynajmem.",
+    icon: LuWarehouse,
+  },
+  {
+    title: "Wywóz odpadów i gabarytów",
+    text: "Szybkie pozbywanie się starych mebli i wielkogabarytowych śmieci bez wysiłku z Twojej strony.",
+    icon: LuTrash2,
+  },
+  {
+    title: "Oczyszczanie piwnic, strychów i garaży",
+    text: "Gruntowne sprzątanie i usuwanie zbędnego wyposażenia, które pozwoli Ci odzyskać wolną przestrzeń.",
+    icon: FaBroom,
+  },
+];
