704f957c1a32d1f659f046ea3ada7686f9579d41
Sandra <sandra.mstowskaa@gmail.com>
2026-07-03 15:23:16 +0200

    style(WorkProcess): optimize mobile text spacing, image ordering and card alignment

--- src/components/StepCard.jsx
+++ src/components/StepCard.jsx
@@ -1,12 +1,12 @@
 const StepCard = ({ num, text }) => {
-    return (
-        <div className="flex items-center justify-center bg-white border-4 border-[#C2B7B2] rounded-[36px] px-4 py-6 text-center transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl shadow-md w-full max-w-85 min-h-40 ">
-            <p className="font-mont text-xs sm:text-sm font-extrabold text-[#353436] uppercase tracking-wide leading-relaxed max-w-65">
-                <span className="text-[#D46B43] mr-1.5 font-bold text-xl">{num}</span>
-                {text}
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center bg-white border-4 border-[#C2B7B2] rounded-[36px] px-4 py-6 text-center transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl shadow-md w-full max-w-85 min-h-40 mx-auto">
+      <p className="font-mont sm:text-sm font-extrabold text-[#353436] uppercase tracking-wide leading-relaxed max-w-65">
+        <span className="text-[#D46B43] mr-1.5 font-bold text-xl">{num}</span>
+        {text}
+      </p>
+    </div>
+  );
 };
 
 export default StepCard;
--- src/components/WorkProcess.jsx
+++ src/components/WorkProcess.jsx
@@ -5,7 +5,7 @@ import Reveal from "../utils/Reveal";
 const WorkProcess = () => {
   return (
     <section className="bg-[#F1EAE6]  pt-12 pb-8 lg:pt-14 font-text px-6 lg:px-12">
-      <Reveal className="max-w-6xl mx-auto py-24 text-center uppercase">
+      <Reveal className="max-w-6xl mx-auto py-8 md:py-24 text-center uppercase">
         <p className="text-[#D46B43] font-bold mb-2 tracking-wide">
           Jak pracujemy?
         </p>
@@ -18,21 +18,21 @@ const WorkProcess = () => {
         delay={0.2}
         className="max-w-6xl mx-auto flex flex-col lg:flex-row justify-around items-center gap-8 lg:gap-4"
       >
-        <div className="w-full lg:w-[20%] flex flex-col gap-6 lg:gap-40 order-2 lg:order-1 text-center">
+        <div className="w-full lg:w-[20%] flex flex-col items-center gap-6 lg:gap-40 order-2 lg:order-1 text-center">
           <StepCard num="1." text="Umowę zawieramy telefonicznie" />
-          <StepCard num="3." text="Bezpiecznie transportujemy i wnosimy" />
+          <StepCard num="2." text="Wykonujemy usługę szybko i profesjonalnie" />
         </div>
 
-        <div className="w-full lg:w-[60%] flex justify-center order-1 lg:order-2">
+        <div className="w-full lg:w-[70%] flex justify-center order-1 lg:order-2">
           <img
             src="/assets/photos.webp"
             alt="4 kroki przeprowadzkowe"
-            className="w-full max-w-145 sm:max-w-175 lg:max-w-210 h-auto drop-shadow-sm transition-all duration-400 hover:-translate-y-1 hover:scale-105"
+            className="w-full max-w-160 sm:max-w-175 lg:max-w-210 h-auto drop-shadow-sm transition-all duration-400 hover:-translate-y-1 hover:scale-105"
           />
         </div>
 
-        <div className="w-full lg:w-[20%] flex flex-col gap-6 lg:gap-40 order-3 lg:order-3 text-center">
-          <StepCard num="2." text="Wykonujemy usługę szybko i profesjonalnie" />
+        <div className="w-full lg:w-[20%] flex flex-col items-center gap-6 lg:gap-40 order-3 lg:order-3 text-center">
+          <StepCard num="3." text="Bezpiecznie transportujemy i wnosimy" />
           <StepCard num="4." text="Cieszysz się z efektów naszej pracy" />
         </div>
       </Reveal>
