5ff5514f1d3fae6c2de9c9761ca4e249eefad9d2
Sandra <sandra.mstowskaa@gmail.com>
2026-07-09 17:12:23 +0200

    style(CitiesPills): downscale padding, border, and font size for mobile screens

--- src/components/CitiesPills.jsx
+++ src/components/CitiesPills.jsx
@@ -1,10 +1,11 @@
-
 const CitiesPills = ({ city }) => {
-    return (
-        <div className="bg-white border-3 border-[#C2B7B2] rounded-full px-6 py-4 shadow-md hover:text-[#D46B43] hover:scale-105 hover:shadow-2xl transition-all duration-300">
-            <p className="font-mont uppercase text-xs font-extrabold tracking-widest">{city}</p>
-        </div>
-    );
+  return (
+    <div className="bg-white border-2 sm:border-3 border-[#C2B7B2] rounded-full px-4 py-2.5 sm:px-6 sm:py-4 shadow-md hover:text-[#D46B43] hover:scale-105 hover:shadow-2xl transition-all duration-300">
+      <p className="font-mont uppercase text-[10px] sm:text-xs font-extrabold tracking-widest text-center">
+        {city}
+      </p>
+    </div>
+  );
 };
 
 export default CitiesPills;
