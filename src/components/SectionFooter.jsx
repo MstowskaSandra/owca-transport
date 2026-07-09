33c8b129fca3f0991b22eadc6183df1df9076344
Sandra <sandra.mstowskaa@gmail.com>
2026-07-09 16:22:11 +0200

    style(SectionFooter): fix center alignment for text elements on mobile devices

--- src/components/SectionFooter.jsx
+++ src/components/SectionFooter.jsx
@@ -3,9 +3,10 @@ import CallButton from "./CallButton";
 const SectionFooter = ({ title, subtext }) => {
   return (
     <div className="pt-20 pb-8 flex flex-col sm:flex-row items-center justify-center lg:justify-end gap-3 font-mont text-sm font-semibold text-[#353436]">
-      <span className="text-[#D46B43] font-bold text-md">{title}</span>
-      <p className="max-w-xl">{subtext}</p>
-
+      <span className="text-[#D46B43] font-bold text-md text-center sm:text-left">
+        {title}
+      </span>
+      <p className="max-w-xl text-center sm:text-left">{subtext}</p>
       <CallButton variant="orange" text="Zadzwoń" />
     </div>
   );
